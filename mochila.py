751b75143208b7880eb209db971924b18299c222
Kurlok <felipe.augum@gmail.com>
2020-10-06 01:28:32 -0300

    Implementação crossover uniforme

--- mochila.py
+++ mochila.py
@@ -16,7 +16,7 @@ def set_apenas_float(msg):
     while True:
         try:
             num = float(input(msg))
-            while num <= 0:
+            while num < 0:
                 print('Por favor, digite um valor float maior ou igual a 0') 
                 num = set_apenas_float((msg))
             break
@@ -98,6 +98,47 @@ def selecao_ranking(fitness, qtd_pais, geracao):
     print('Pais selecionados:\n {}'.format(pais))
     return pais
 
+def crossover_uniforme(pais, qtd_filhos):
+    print ('\nCROSSOVER UNIFORME')
+    filhos = npy.empty((qtd_filhos, pais.shape[1])) #pais.shape[0] é a qtd_pais e pais.shape[1] é a qtd_items
+    filhos = filhos.astype(int)
+    i=0
+    while (i < qtd_filhos):
+        print ('------------------Cruzamento {}------------------'.format(i+1))    
+        #Sorteando os pais que irão reproduzir
+        pai1_idx = rd.randint(0, pais.shape[0]-1)
+        pai2_idx = rd.randint(0, pais.shape[0]-1)
+        while (pai2_idx == pai1_idx) and (pais.shape[0]!=1): 
+            pai2_idx = rd.randint(0, pais.shape[0]-1)
+        print ('Índice escolhido do pai 1: {}'.format(pai1_idx))
+        print ('Índice escolhido do pai 2: {}'.format(pai2_idx))
+        print ('Pai 1: {}'.format(pais[pai1_idx]))
+        print ('Pai 2: {}'.format(pais[pai2_idx]))        
+        sorteio_crossover = round(rd.random(),4)
+        print('Taxa de crossver: {0}.  O número sorteado para crossover foi: {1}'.format(crossover_taxa,sorteio_crossover))
+        if sorteio_crossover <= crossover_taxa:
+            for j in range(pais.shape[1]): #mutantes.shape[1] = qtd_itens
+                sorteio_gene = rd.choice([1, 0])
+                if sorteio_gene == 1:
+                    filhos[i,j] = pais[pai1_idx][j]
+                    print ('O crossover uniforme do gene {} ocorreu. Ele herdará o gene do pai 1'.format(j+1))
+                else:
+                    filhos[i,j] = pais[pai2_idx][j]
+                    print ('O crossover uniforme do gene {} ocorreu. Ele herdará o gene do pai 2'.format(j+1))
+            print ('Filho depois do crossover uniforme: {}'.format(filhos[i]))
+        else:
+            print('O crossover não ocorreu! Os genes de um dos pais continuará nos filhos')
+            pai_escolhido = rd.choice([pai1_idx, pai2_idx])
+            if (pai_escolhido == pai1_idx):
+                filhos[i] = pais[pai1_idx]
+                print('O filho herdou os genes do pai 1: {}'.format(filhos[i]))
+            else:
+                filhos[i] = pais[pai2_idx]
+                print('O filho herdou os genes do pai 2: {}'.format(filhos[i]))          
+        i = i+1
+    print ('Filhos Gerados:\n {}'.format(filhos))
+    return filhos    
+
 def crossover_um_ponto(pais, qtd_filhos):
     print ('\nCROSSOVER EM UM PONTO')
     filhos = npy.empty((qtd_filhos, pais.shape[1])) #pais.shape[0] é a qtd_pais e pais.shape[1] é a qtd_items
@@ -158,7 +199,7 @@ def mutacao(filhos):
         print ('Filho depois da mutação: {}'.format(mutantes[i]))
     return mutantes   
 
-def otimizar(peso, valor, geracao, qtd_individuos, qtd_geracoes, qtd_pais, peso_max_mochila):
+def otimizar(peso, valor, geracao, qtd_individuos, qtd_geracoes, qtd_pais, peso_max_mochila, tipo_crossover, tipo_selecao):
     fitness_historico = []
     qtd_filhos = qtd_individuos - qtd_pais 
     max_fitness = 0
@@ -166,31 +207,38 @@ def otimizar(peso, valor, geracao, qtd_individuos, qtd_geracoes, qtd_pais, peso_
         print('\nGERAÇÃO {0}: \n{1}'.format(i, geracao)) 
         fitness = calcula_fitness(peso, valor, geracao, peso_max_mochila, i)
         fitness_historico.append(fitness)
-        #Verificando qual o maior fitness até agorao
+        #Verificando qual o maior fitness até agora
         if (npy.max(fitness) > max_fitness):
             max_fitness = npy.max(fitness)
-        # pais = selecao_roleta(fitness, qtd_pais, geracao)
-        pais = selecao_ranking(fitness, qtd_pais, geracao)
-        filhos = crossover_um_ponto(pais, qtd_filhos)
+        if tipo_selecao == 1:
+            pais = selecao_ranking(fitness, qtd_pais, geracao)
+        elif tipo_selecao == 2: 
+            pais = selecao_roleta(fitness, qtd_pais, geracao)
+        if tipo_crossover == 1:
+            filhos = crossover_uniforme(pais, qtd_filhos)
+        elif tipo_crossover == 2: 
+            filhos = crossover_um_ponto(pais, qtd_filhos)
         mutantes = mutacao(filhos)
         geracao[0:pais.shape[0], :] = pais
         geracao[pais.shape[0]:, :] = mutantes
     return fitness_historico
 
-#Definindo as variáveis
+#Variáveis para teste
 mochila_peso_max = 300
 qtd_itens = 6
 peso_item_min = 10
 peso_item_max = 100
 valor_item_min = 15
 valor_item_max = 150
-qtd_geracoes = 10
+qtd_geracoes = 3
 qtd_individuos = 6 #É a quantidade de indivíduos para o problema
 qtd_pais = 3
 crossover_taxa = 0.8
 mutacao_taxa = 0.05
+tipo_selecao = 1 # 1 para Ranking e 2 para Roleta
+tipo_crossover = 1 # 1 para Uniforme e 2 para Um ponto
 
-#Entrada para as variáveis
+#Entrada para as variáveis 
 mochila_peso_max = set_apenas_int("Digite o peso máximo que a mochila aguenta:\n")
 qtd_itens = set_apenas_int(("Digite a quantidade de itens que serão combinadas para colocar na mochila:\n"))
 peso_item_min = set_apenas_int(("Digite o peso mínimo para o item:\n"))
@@ -208,6 +256,14 @@ while valor_item_max < valor_item_min:
 qtd_individuos = set_apenas_int(("Digite a quantidade de indivíduos:\n"))
 qtd_pais = set_apenas_int(("Digite a quantidade de pais:\n"))
 qtd_geracoes = set_apenas_int(("Digite a quantidade de gerações:\n"))
+tipo_selecao = set_apenas_int(("Escolha o tipo de seleção:\n1- Ranking\n2- Roleta"))
+while (tipo_selecao != 1) and (tipo_selecao != 2):
+    print('Digite escolha um valor válido. Digite 1 para Seleção Ranking ou 2 para Seleção Roleta') 
+    tipo_selecao = set_apenas_int(("Escolha o tipo de seleção:\n1- Ranking\n2- Roleta"))
+tipo_crossover = set_apenas_int(("Escolha o tipo de crossover:\n1- Uniforme\n2- Um ponto"))
+while (tipo_crossover != 1) and (tipo_crossover != 2):
+    print('Digite escolha um valor válido. Digite 1 para Crossover Uniforme ou 2 para Crossover Em Um Ponto') 
+    tipo_crossover = set_apenas_int(("Escolha o tipo de crossover:\n1- Uniforme\n2- Um ponto"))
 crossover_taxa = set_apenas_float(("Digite a taxa de crossover:\n"))
 mutacao_taxa = set_apenas_float(("Digite a taxa de mutação:\n"))
 
@@ -233,6 +289,14 @@ print('Valor máximo que um item pode possuir: {}'.format(valor_item_max))
 print('Quantidade de indivíduos por geração: {}'.format(qtd_individuos))
 print('Quantidade de gerações: {}'.format(qtd_geracoes))
 print('Quantidade de pais: {}'.format(qtd_pais))
+if tipo_selecao == 1:
+    print('Tipo de seleção: Ranking')
+elif tipo_selecao == 2: 
+    print('Tipo de seleção: Roleta')
+if tipo_crossover == 1:
+    print('Tipo de crossover: Uniforme')
+elif tipo_crossover == 2: 
+    print('Tipo de crossover: Um ponto')
 print('Taxa de crossover: {}'.format(crossover_taxa))
 print('Taxa de mutação: {}'.format(mutacao_taxa))
 print('\nLISTA DE ITENS')
@@ -242,7 +306,7 @@ for i in range(qtd_itens):
 print('\nFUNÇÃO FITNESS\nSe o somatório de (peso do item * gene) para todos os genes do indivíduo da geração for menor ou igual ao peso máximo que a mochila pode aguentar então o fitness é igual o somatório (valor do item * gene) (peso do item * gene) para todos os genes do indivíduo. Caso contrário o fitness é igual a 0.')
 print('\nGERAÇÕES\n0 ou 1 significa se o item está presente ou não no indivíduo de cada geração:')
 
-fitness_historico = otimizar(peso, valor, populacao_inicial, qtd_individuos, qtd_geracoes, qtd_pais, mochila_peso_max)
+fitness_historico = otimizar(peso, valor, populacao_inicial, qtd_individuos, qtd_geracoes, qtd_pais, mochila_peso_max, tipo_crossover, tipo_selecao)
 
 # Geração do gráfico
 fitness_media = [npy.mean(fitness) for fitness in fitness_historico]
