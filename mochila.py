e171a891fcc8f8fd0ca9594afe28b53a756d8f26
Kurlok <felipe.augum@gmail.com>
2020-10-06 00:35:01 -0300

    Ajustes de formatação

--- mochila.py
+++ mochila.py
@@ -58,6 +58,7 @@ def selecao_roleta(fitness, qtd_pais, geracao):
     print('Índices dos indivíduo correspondentes ordenados: {}'.format(indices_roleta_ordenados))    
     indice_selecionado_pai = 0
     pais = npy.empty((qtd_pais, geracao.shape[1])) #geracao.shape[1] é equivalente a qtd_itens
+    pais = pais.astype(int)
     matriz_roleta = [[0 for i in range(fitness.size)] for i in range(2)] #Matriz 2 x qtd_itens onde a primeira linha é o índice do indivíduo e a segunda o valor da porcentagem do fitness.
     i = 0
     soma_roleta = 0
@@ -88,6 +89,7 @@ def selecao_ranking(fitness, qtd_pais, geracao):
     print('Indices Fitness Ordenados Decrescentemente: {}'.format(indices_fitness_ordenados))    
     indice_selecionado_pai = 0
     pais = npy.empty((qtd_pais, geracao.shape[1])) #geracao.shape[1] é equivalente a qtd_itens
+    pais = pais.astype(int)
     i = 0
     for i in range(qtd_pais):
         indice_selecionado_pai = indices_fitness_ordenados[i]
@@ -99,6 +101,7 @@ def selecao_ranking(fitness, qtd_pais, geracao):
 def crossover_um_ponto(pais, qtd_filhos):
     print ('\nCROSSOVER EM UM PONTO')
     filhos = npy.empty((qtd_filhos, pais.shape[1])) #pais.shape[0] é a qtd_pais e pais.shape[1] é a qtd_items
+    filhos = filhos.astype(int)
     crossover_ponto = int(pais.shape[1]/2) #O ponto escolhido é a metade da indivíduo
     i=0
     while (i < qtd_filhos):
@@ -135,6 +138,7 @@ def crossover_um_ponto(pais, qtd_filhos):
 def mutacao(filhos):
     print ('\nMUTAÇÃO NOS FILHOS')
     mutantes = npy.empty((filhos.shape))
+    mutantes = mutantes.astype(int)
     mutacao_taxa = 0.05
     for i in range(mutantes.shape[0]): #mutantes.shape[0] = qtd_filhos
         print ('------------------Mutação {}------------------'.format(i+1))
@@ -142,7 +146,7 @@ def mutacao(filhos):
         print ('Filho antes da mutação: {}'.format(mutantes[i]))
         for j in range(mutantes.shape[1]): #mutantes.shape[1] = qtd_itens
             sorteio_mutacao = round(rd.random(),4)
-            print('Taxa de mutação: {0}.  O número sorteado para a mutação foi: {1}'.format(mutacao_taxa,sorteio_mutacao))
+            print('Taxa de mutação: {0}. O número sorteado para a mutação foi: {1}'.format(mutacao_taxa,sorteio_mutacao))
             if sorteio_mutacao > mutacao_taxa:
                 print ('A mutação do gene {} não ocorreu'.format(j+1))
                 continue
@@ -180,30 +184,32 @@ peso_item_min = 10
 peso_item_max = 100
 valor_item_min = 15
 valor_item_max = 150
-qtd_geracoes = 5
-qtd_individuos = 8 #É a quantidade de indivíduos para o problema
-qtd_pais = 5
+qtd_geracoes = 10
+qtd_individuos = 6 #É a quantidade de indivíduos para o problema
+qtd_pais = 3
 crossover_taxa = 0.8
 mutacao_taxa = 0.05
 
 #Entrada para as variáveis
-mochila_peso_max = set_apenas_int("Digite o peso máximo que a mochila aguenta:\n")
-qtd_itens = set_apenas_int(("Digite a quantidade de itens que serão combinadas para colocar na mochila:\n"))
-peso_item_min = set_apenas_int(("Digite o peso mínimo para o item:\n"))
-peso_item_max  = 0
-while peso_item_max <= peso_item_min:
-    print('O peso máximo do item não pode ser inferior ao peso mínimo') 
-    peso_item_max  = set_apenas_int(("Digite o peso máximo para o item:\n"))
-valor_item_min = set_apenas_int(("Digite o valor mínimo para o item:\n"))
-valor_item_max = 0
-while valor_item_max <= valor_item_min:
-    print('O valor máximo do item não pode ser inferior ao valor mínimo') 
-    valor_item_max = set_apenas_int(("Digite o valor máximo para o item:\n"))
-qtd_individuos = set_apenas_int(("Digite a quantidade de indivíduos:\n"))
-qtd_pais = set_apenas_int(("Digite a quantidade de pais:\n"))
-qtd_geracoes = set_apenas_int(("Digite a quantidade de gerações:\n"))
-crossover_taxa = set_apenas_float(("Digite a taxa de crossover:\n"))
-mutacao_taxa = set_apenas_float(("Digite a taxa de mutação:\n"))
+# mochila_peso_max = set_apenas_int("Digite o peso máximo que a mochila aguenta:\n")
+# qtd_itens = set_apenas_int(("Digite a quantidade de itens que serão combinadas para colocar na mochila:\n"))
+# peso_item_min = set_apenas_int(("Digite o peso mínimo para o item:\n"))
+# peso_item_max = 0
+# peso_item_max  = set_apenas_int(("Digite o peso máximo para o item:\n"))
+# while peso_item_max < peso_item_min:
+#     print('O peso máximo do item não pode ser inferior ao peso mínimo') 
+#     peso_item_max  = set_apenas_int(("Digite o peso máximo para o item:\n"))
+# valor_item_min = set_apenas_int(("Digite o valor mínimo para o item:\n"))
+# valor_item_max = 0
+# valor_item_max = set_apenas_int(("Digite o valor máximo para o item:\n"))
+# while valor_item_max < valor_item_min:
+#     print('O valor máximo do item não pode ser inferior ao valor mínimo') 
+#     valor_item_max = set_apenas_int(("Digite o valor máximo para o item:\n"))
+# qtd_individuos = set_apenas_int(("Digite a quantidade de indivíduos:\n"))
+# qtd_pais = set_apenas_int(("Digite a quantidade de pais:\n"))
+# qtd_geracoes = set_apenas_int(("Digite a quantidade de gerações:\n"))
+# crossover_taxa = set_apenas_float(("Digite a taxa de crossover:\n"))
+# mutacao_taxa = set_apenas_float(("Digite a taxa de mutação:\n"))
 
 #Vetor com o número do item
 n_item = npy.arange(1,qtd_itens+1)
